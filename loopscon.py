ae8fba932e5c145df2ff3ee482c621512b397f51
Meriki solomon <elmeriki@gmail.com>
2020-01-13 06:25:08 +0200

    new changes

---
+++ loopscon.py
@@ -0,0 +1,10 @@
+# numbers = [9,20,382,4747,3838,273646,3625,47462]
+# for i in numbers:
+#     print(i)
+
+# Range is use to specify a specific range of values if you no where u want it to start and stop
+
+for i in range(6):
+    print(i)
+
+
