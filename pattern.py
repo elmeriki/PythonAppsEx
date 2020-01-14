3a830f18bfd302faa97894a5d61de25803051f81
Meriki solomon <elmeriki@gmail.com>
2020-01-14 11:21:03 +0200

    New changes

---
+++ pattern.py
@@ -0,0 +1,52 @@
+# for i  in range(4):
+#     print("*" , end="")
+    
+# print()
+
+# for i  in range(4):
+#     print("*" , end="")
+
+# print()
+
+# for i  in range(4):
+#     print("*" , end="")
+
+# print()
+
+# for i  in range(4):
+#     print("*" , end="")
+
+# Ok but tomuch repeating
+
+# for i  in range(4):
+#     for i  in range(4):
+#             print("*" , end="")
+#     print()
+
+# # # #
+# # # #
+# # # #
+# # # #
+
+
+# for i  in range(6):
+#     for j  in range(i):
+#             print("*" , end="")
+#     print()
+
+#
+# #
+# # #
+# # # #
+# # # # #
+
+
+for i  in range(6):
+    for j  in range(4-i):
+            print("*" , end="")
+    print()
+    
+####
+###
+##
+#
