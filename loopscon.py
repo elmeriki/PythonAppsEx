ebfdb4d7d5e18795576477c4d6b8932e59abdf30
Meriki solomon <elmeriki@gmail.com>
2020-01-14 08:59:08 +0200

    New changes

--- loopscon.py
+++ loopscon.py
@@ -1,10 +1,70 @@
+import math
 # numbers = [9,20,382,4747,3838,273646,3625,47462]
 # for i in numbers:
 #     print(i)
 
-# Range is use to specify a specific range of values if you no where u want it to start and stop
+# Range is use to specify a specific range of values if you no where u want it to start and stop (for loops also 
+# allows you to specify setting range to print )
 
-for i in range(6):
-    print(i)
+# Starting Point
+# Ending Point and  
+# Alteration  
 
 
+# for i in range(90):
+#     print(i)
+
+
+# for solomon  in range(1,20,5):
+#     print(solomon)
+
+
+
+# for i in range (-10, 10):
+#     print(i)
+
+# names = ["solomon", "paul", "mary"]
+# for x in names:
+#   if x == "hhaahaha":
+#     break
+#   print(x) 
+
+
+# for i in range(10):
+#     if i % 2!=0:
+#         print(i)
+        
+# Assignment: 
+    
+# WRite a code that wil print all the quare number between 1-500
+
+# for sqnum in range(1,500):
+#     x=math.sqrt(sqnum)
+#     if sqnum% x==0:
+#         print(sqnum)
+
+
+    # BREAK | CONTINUE | AND PASS  
+    
+# x = int(input("How many Balls do you want? "))
+# i=1
+# while i <= x:
+#     print("Ball ", i)
+#     i = i+1
+    
+
+# x= int(input("How many Sweet do you wan to buy? "))
+# i=1
+# avs =4
+# while i <= x:
+#     if i>avs:
+#         print("Sweet Left ", i )
+#         break
+#     print("Sweet")
+#     i=i+1
+
+
+for i in range (100):
+    if i%3!=0 and i%5 !=0:
+        print(i)
+        
